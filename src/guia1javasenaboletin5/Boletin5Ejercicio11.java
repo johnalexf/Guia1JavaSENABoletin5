8c4597d971cd0db6d8848646842efa3d37a13a5f
johnalexf <johnalexfr@gmail.com>
2026-04-16 17:35:55 -0500

    refactor: Estructura base y modularidad para el Ejercicio 11
    - Creación del esqueleto inicial de la clase Boletin5Ejercicio11.
    - Separación de la lógica en bloques independientes (armarStringMenu, mostrarError).
    - Preparación de variables globales para su posterior implementación.

--- src/guia1javasenaboletin5/Boletin5Ejercicio11.java
+++ src/guia1javasenaboletin5/Boletin5Ejercicio11.java
@@ -11,36 +11,64 @@ package guia1javasenaboletin5;
  */
 public class Boletin5Ejercicio11 {
     
-    public static void solucion(){
+    /*
+    11-Se pretende realizar un programa para gestionar la lista de participaciones en una competición de 
+        salto de longitud. El número de plazas disponible es de 10. Sus datos se irán introduciendo en el 
+        mismo orden que vayan inscribiéndose los atletas. Diseñar el programa que muestre las siguientes 
+        opciones:
+        1- Inscribir un participante.
+        2- Mostrar listado de datos.
+        3- Mostrar listado por marcas.
+        4- Finalizar el programa.
+        Si se selecciona 1, se introducirán los datos de uno de los participantes: Nombre, mejor marca del 
+        2002, mejor marca del 2001 y mejor marca del 2000.
+        Si se elige la opción 2, se debe mostrar un listado por número de dorsal.
+        La opción 3 mostrará un listado ordenado por la marca del 2002, de mayor a menor.
+        Tras procesar cada opción, se debe mostrar de nuevo el menú inicial, hasta que se seleccione la 
+        opción 4, que terminará el programa.
+    */
     
-        System.out.println("11-Se pretende realizar un programa para gestionar la lista de participaciones en una competición de \n" +
-                        "   salto de longitud. El número de plazas disponible es de 10. Sus datos se irán introduciendo en el mismo \n" +
-                        "   orden que vayan inscribiéndose los atletas. Diseñar el programa que muestre las siguientes opciones:\n" +
-                        "   1- Inscribir un participante.\n" +
-                        "   2- Mostrar listado de datos.\n" +
-                        "   3- Mostrar listado por marcas.\n" +
-                        "   4- Finalizar el programa.\n" +
-                        "   Si se selecciona 1, se introducirán los datos de uno de los participantes: Nombre, mejor marca del \n" +
-                        "   2002, mejor marca del 2001 y mejor marca del 2000.\n" +
-                        "   Si se elige la opción 2, se debe mostrar un listado por número de dorsal.\n" +
-                        "   La opción 3 mostrará un listado ordenado por la marca del 2002, de mayor a menor.\n" +
-                        "   Tras procesar cada opción, se debe mostrar de nuevo el menú inicial, hasta que se seleccione la opción \n" +
-                        "   4, que terminará el programa.\n\n\n\n");
-        
-        String[] menu;
-        menu = new String[]{
+    
+    //Variables y funcion para estructurar el menu de opciones
+    public static String[] menu;
+    public static StringBuilder concatenarMenu = new StringBuilder();
+    
+    public static void armarStringMenu(){
+    
+         menu = new String[]{
             "Inscribir un participante.",
             "Mostrar listado de datos.",
             "Mostrar listado por marcas.",
             "Finalizar el programa."
         };
         
-        StringBuilder concatenarMenu = new StringBuilder();
-        
         concatenarMenu.append("\n Menu: \n");
         for( int i=0; i< menu.length; i++){
             concatenarMenu.append(i+1).append(".-").append(menu[i]).append("\n");
         }
+  
+    }
+    //Fin de Variables y funcion para estructurar el menu de opciones
+   
+    
+    public static void solucion(){
+
+        armarStringMenu();
+        
+        int cantidadAtletas = 10;
+        int cantidadAtletasGuardados = 0;
+        int cantMarcas = 3;
+        String[] nombreAtleta = new String[cantidadAtletas];
+        int[] numeroDorsal = new int[cantidadAtletas];
+        int limiteNumDorsal = 9999;
+        int[][] marcas = new int[cantidadAtletas][cantMarcas];
+        /*La posiciones de las mejores marcas se manejaran asi:
+            indice 0 = mejor marca del año 2002
+            indice 1 = mejor marca del año 2001
+            indice 2 = mejor marca del año 2000
+        */
+        String[] añoMarcas = new String[]{"2002","2001","2000"};
+        
         
         int opcion=0;
         while(opcion !=4){
@@ -52,6 +80,37 @@ public class Boletin5Ejercicio11 {
             switch(opcion){
                 
                 case 1:
+                    if(cantidadAtletasGuardados < cantidadAtletas){
+                        System.out.printf("%nA continuacion va escribir la informacion del atleta #%d %n", cantidadAtletasGuardados + 1 );
+                        System.out.printf("Nombre del atleta : ");
+                        nombreAtleta[cantidadAtletasGuardados] = Entrada.texto();
+ 
+                        while (true){
+                            System.out.printf("%nEscribe el NUMERO del atleta\n");
+                            System.out.printf("Recuerda debe estar en un rango de 1 hasta %d\n", limiteNumDorsal);
+                            numeroDorsal[cantidadAtletasGuardados] = Entrada.entero();
+
+                            if(numeroDorsal[cantidadAtletasGuardados] > limiteNumDorsal){
+                                mostrarError("Valor por encima del permitido");
+                            }else{
+                                break;
+                            }                             
+                        }
+                        
+                        System.out.println("");
+                        for(int i=0 ; i<cantMarcas ; i++){
+                                                           
+                            System.out.printf("Mejor marca del año %s en cm :",añoMarcas[i]);
+                            marcas[cantidadAtletasGuardados][i] = Entrada.entero(false, false, true);
+
+                        }
+                        
+                        cantidadAtletasGuardados++;
+                        
+                    }else{
+                        System.out.printf("Ha llegado al limite de %d atletas \n", cantidadAtletas);
+                    }
+                    
                     break;
                 
                 case 2:
@@ -64,9 +123,7 @@ public class Boletin5Ejercicio11 {
                     break;
                 
                 default:
-                    System.out.println("\n-----------------------------------------");
-                    System.out.println("ERROR: Opcion no valida, intente de nuevo");
-                    System.out.println("-----------------------------------------\n");
+                    mostrarError("Opcion no valida, intente de nuevo");
                     break;
             
             }
@@ -76,5 +133,11 @@ public class Boletin5Ejercicio11 {
         
         System.out.println("Fin del programa");
     
+    }
+    
+    public static void mostrarError(String error) {
+        System.out.println("\n-----------------------------------------");
+        System.out.println("Error: "+ error);
+        System.out.println("-----------------------------------------\n");
     }
 }
